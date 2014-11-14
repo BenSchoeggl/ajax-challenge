7cd45afb5ebc57134fc2a1096609ad99ab3c6eae
Ben Schoeggl <benschoeggl@icloud.com>
2014-11-14 13:15:27 -0800

    implemented bootstrap for the top part and started the angular controller for the form

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
 "use strict";
 /*
     app.js, main Angular application script
-    define your module and controllers here
 */
+
+angular.module('ReviewHandler', ['ui.bootstrap'])
+    .config(function($httpProvider) {
+        // configures the module to talk to the database I have setup on parse.com for the website
+        $httpProvider.defaults.headers.common['X-Parse-Application-Id'] = 'r08kYhI1EpXVsSxo5UNA1XhB29XCNsnVCpZbOolY';
+        $httpProvider.defaults.headers.common['X-Parse-REST-API-Key'] = '09pXpIgpA0JrexzlP0JZ2xpnzkEZvDSTTzRkMRDy';
+    })
+    .controller('ReviewController', function($scope, $http) {
+        var reviewUrl = 'https://api.parse.com/1/classes/Review';
+        $scope.refreshReviews = function() {
+
+        }
+    });
