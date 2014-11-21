e4adcd91fd0ff47cec9c0523e51e9542c7a47035
Ben Schoeggl <benschoeggl@icloud.com>
2014-11-21 10:57:22 -0800

    switched to table for review feed. Divs taking too much time.

--- js/app.js
+++ js/app.js
@@ -10,14 +10,34 @@ angular.module('ReviewHandler', ['ui.bootstrap'])
         $httpProvider.defaults.headers.common['X-Parse-REST-API-Key'] = '09pXpIgpA0JrexzlP0JZ2xpnzkEZvDSTTzRkMRDy';
     })
     .controller('ReviewController', function($scope, $http) {
-        var reviewUrl = 'https://api.parse.com/1/classes/Review';
+        var reviewsURL = 'https://api.parse.com/1/classes/Review';
+        // Sets scope.reviewGroupsBy3 to an array of arrays each containing 3 review objects so they can be put into
+        // divs by ng-repeat
         $scope.refreshReviews = function() {
-            $scope.loading = true;
-            $http.get(reviewUrl + '?where={"done": false}')
+            // Add $scope.loading = true;
+            $scope.reviewGroupsBy3 = [];
+            $http.get(reviewsURL + '?where={"done": false}')
                 .sucess(function(responseData) {
-                    $scope.reviewGroup = responseData.results;
-
-                    // add for loop group by 3 using mod
+                    var resultsIdx = 0;
+                    var i;
+                    var j;
+                    var arrayToPush;
+                    var reviewGroupsBy3Size = responseData.results.length / 3;
+                    if (responseData.results.length % 3 > 0) {
+                        reviewGroupsBy3Size++;
+                    }
+                    for (i = 0; i < reviewGroupsBy3Size; i++) {
+                        arrayToPush = [];
+                        j = 0;
+                        while (j < 3 && resultsIdx < responseData.results.length) {
+                            arrayToPush.push(responseData.results[resultsIdx]);
+                            resultsIdx++;
+                            j++
+                        }
+                        reviewGroupsBy3[i].push(arrayToPush);
+                        i++;
+                    }
+                    console.log($scope.reviewGroupsBy3);
                 })
                 .error(function(err) {
                     console.log(err);
@@ -25,15 +45,19 @@ angular.module('ReviewHandler', ['ui.bootstrap'])
                 .finally(function() {
                     $scope.loading = false;
                 });
-        };
+        }; // refreshReviews
 
-        $scope.refreshReviews();
+        $scope.refreshReviews(); // called to refresh reviews when the page loads
+        $scope.newReview = {votes: 0}; // initialize a new review object on the scope for the new review form
+
+        // Posts a new review to the parse.com server
         $scope.addReview = function(newReview) {
             $scope.inserting = true;
-            $http.post(reviewUrl, newReview)
+            $http.post(reviewsURL, newReview)
                 .success(function(responseData) {
                     newReview.objectId = responseData.objectId;
-                    $scope.reviewGroup.push(newReview);
+                    $scope.push(newReview);
+                    $scope.newReview = {votes: 0};
                 })
                 .error(function (err) {
                     console.log(err);
@@ -41,11 +65,12 @@ angular.module('ReviewHandler', ['ui.bootstrap'])
                 .finally(function() {
                     $scope.inserting = false;
                 });
+            // end of $http.post
         };
 
         $scope.updateReview = function(review) {
             $scope.updating = true;
-            $http.put(reviewUrl + '/' + review.objectId, review)
+            $http.put(reviewsURL + '/' + review.objectId, review)
                 .success(function(responseData){})
                 .error(function(err) {
                     console.log(err);
