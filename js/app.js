3666cc2f1285f07cf627be999982e4c358cb075f
Ben Schoeggl <benschoeggl@icloud.com>
2014-11-21 15:28:47 -0800

    working on error where the scope does not recognize the refreshReviews method

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
     app.js, main Angular application script
 */
 
-angular.module('ReviewHandler', ['ui.bootstrap'])
+angular.module('Reviews and Ratings', ['ui.bootstrap'])
     .config(function($httpProvider) {
         // configures the module to talk to the database I have setup on parse.com for the website
         $httpProvider.defaults.headers.common['X-Parse-Application-Id'] = 'r08kYhI1EpXVsSxo5UNA1XhB29XCNsnVCpZbOolY';
@@ -11,39 +11,17 @@ angular.module('ReviewHandler', ['ui.bootstrap'])
     })
     .controller('ReviewController', function($scope, $http) {
         var reviewsURL = 'https://api.parse.com/1/classes/Review';
-        // Sets scope.reviewGroupsBy3 to an array of arrays each containing 3 review objects so they can be put into
-        // divs by ng-repeat
         $scope.refreshReviews = function() {
             // Add $scope.loading = true;
-            $scope.reviewGroupsBy3 = [];
-            $http.get(reviewsURL + '?where={"done": false}')
+            $http.get(reviewsURL)
                 .sucess(function(responseData) {
-                    var resultsIdx = 0;
-                    var i;
-                    var j;
-                    var arrayToPush;
-                    var reviewGroupsBy3Size = responseData.results.length / 3;
-                    if (responseData.results.length % 3 > 0) {
-                        reviewGroupsBy3Size++;
-                    }
-                    for (i = 0; i < reviewGroupsBy3Size; i++) {
-                        arrayToPush = [];
-                        j = 0;
-                        while (j < 3 && resultsIdx < responseData.results.length) {
-                            arrayToPush.push(responseData.results[resultsIdx]);
-                            resultsIdx++;
-                            j++
-                        }
-                        reviewGroupsBy3[i].push(arrayToPush);
-                        i++;
-                    }
-                    console.log($scope.reviewGroupsBy3);
+                   $scope.reviews = responseData.results;
                 })
                 .error(function(err) {
                     console.log(err);
                 })
                 .finally(function() {
-                    $scope.loading = false;
+                    //$scope.loading = false;
                 });
         }; // refreshReviews
 
@@ -56,19 +34,19 @@ angular.module('ReviewHandler', ['ui.bootstrap'])
             $http.post(reviewsURL, newReview)
                 .success(function(responseData) {
                     newReview.objectId = responseData.objectId;
-                    $scope.push(newReview);
+                    $scope.reviews.push(newReview);
                     $scope.newReview = {votes: 0};
                 })
                 .error(function (err) {
                     console.log(err);
                 })
                 .finally(function() {
-                    $scope.inserting = false;
+                    //$scope.inserting = false;
                 });
             // end of $http.post
         };
 
-        $scope.updateReview = function(review) {
+        /*$scope.updateReview = function(review) {
             $scope.updating = true;
             $http.put(reviewsURL + '/' + review.objectId, review)
                 .success(function(responseData){})
@@ -78,5 +56,5 @@ angular.module('ReviewHandler', ['ui.bootstrap'])
                 .finally(function() {
                     $scope.updating = true;
                 })
-        };
+        };*/
     });
